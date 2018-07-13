d6de98311ceb01b2588c004070c08d4637718ca2
zhouhao <353707800@qq.com>
2018-07-13 14:16:58 +0800

    Initial commit

---
+++ src/com/today/controller/IndexController.kt
@@ -0,0 +1,10 @@
+package com.today.controller
+
+/**
+ * Created with Intellij IDEA.
+ * @author zhouhao
+ * @version 2018/7/13 0013
+ */
+fun main(args: Array<String>) {
+    println("hello kotlin")
+}
